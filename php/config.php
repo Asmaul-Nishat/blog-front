9372c4fbeaa5d889f986a505450c972024a81829
Asmaul Nishat <nishatsoyati@gmail.com>
2025-07-30 05:18:45 +0600

    database connect

--- php/config.php
+++ php/config.php
@@ -1,11 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Starting DB connection test...<br>";
+
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db = 'myblog';
+$db = 'new-blog-database';
 
 $conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Database connected successfully!";
 ?>
